a84564e6d9605d4b49a6ec79c27bc648d9b0242a
Pedro Ulrich <pedroheringer2020@gmail.com>
2023-06-13 16:26:43 -0300

    created admins module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { CompaniesModule } from './companies/companies.module';
+import { AdminsModule } from './admins/admins.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [AuthModule, UsersModule, ProductsModule, CompaniesModule],
+  imports: [AuthModule, UsersModule, ProductsModule, CompaniesModule, AdminsModule],
 })
 export class AppModule {}
