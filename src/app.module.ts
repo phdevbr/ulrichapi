bcc06a2dfa57779b55d4fbd0f1a1598b87720bf2
Pedro Ulrich <pedroheringer2020@gmail.com>
2023-06-13 16:22:49 -0300

    created resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { CompaniesModule } from './companies/companies.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [AuthModule, UsersModule, ProductsModule, CompaniesModule],
 })
 export class AppModule {}
